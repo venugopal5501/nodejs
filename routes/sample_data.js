f0781e2305507c805eb4185d26844825a53324c9
venugopal5501 <venugopal14082002@gmail.com>
2024-08-29 17:44:22 +0530

    yes

--- routes/sample_data.js
+++ routes/sample_data.js
@@ -19,7 +19,30 @@ router.get("/", function (request, response, next) {
 });
 
 router.get("/add", function (request, response, next) {
-    response.send("Add sample data");
+    response.render("samples1", { title: 'Data Insertion', action: 'list' });
+
+
+});
+
+router.post("/add_sample", function (request, response, next) {
+    var No = request.body.No;
+    var Fname = request.body.Fname;
+    var Lname = request.body.Lname;
+    var Phone = request.body.Phone;
+    var Address = request.Address;
+
+    var query = `insert into Customer(Fname,Lname,Phone,Address)
+    values('${Fname}','${Lname}','${Phone}','${Address}')`;
+
+    database.query(query, function (error, data) {
+        if (error) {
+            throw error;
+        }
+        else {
+            response.redirect("/samples1");
+        }
+    })
+
 });
 
 router.get("/ad", function (request, response, next) {

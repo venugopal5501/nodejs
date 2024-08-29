320e6fc9afb06f33d2c369c2becce6e62067b792
venugopal5501 <venugopal14082002@gmail.com>
2024-08-29 16:25:31 +0530

    Done

--- routes/sample_data.js
+++ routes/sample_data.js
@@ -2,8 +2,21 @@ var express = require("express");
 var router = express.Router();
 var database = require('../database');
 router.get("/", function (request, response, next) {
-    response.send("VenuGopal")
-})
+    var query1 = "select *from Customer order by address desc";
+    database.query(query1, function (error, data) {
+        if (error) {
+            throw error;
+        }
+        else {
+            response.render('samples', {
+                title: 'Nodejs Application',
+                action: "list",
+                Data: data.recordset
+            });
+        }
+    });
+
+});
 
 router.get("/add", function (request, response, next) {
     response.send("Add sample data");

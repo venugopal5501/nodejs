34b4962062a5ef1e1169dee34ee552e5cf3a6d36
venugopal5501 <venugopal14082002@gmail.com>
2024-08-30 16:08:42 +0530

    yes

--- routes/sample_data.js
+++ routes/sample_data.js
@@ -8,10 +8,10 @@ router.get("/", function (request, response, next) {
             throw error;
         }
         else {
-            response.render('samples', {
+            response.render('samples1', {
                 title: 'Nodejs Application',
                 action: "list",
-                Data: data.recordset
+                Data: data.recordset,
             });
         }
     });
@@ -19,7 +19,7 @@ router.get("/", function (request, response, next) {
 });
 
 router.get("/add", function (request, response, next) {
-    response.render("samples1", { title: 'Data Insertion', action: 'list' });
+    response.render("samples1", { title: 'Data Insertion', action: 'add' });
 
 
 });
@@ -29,24 +29,45 @@ router.post("/add_sample", function (request, response, next) {
     var Fname = request.body.Fname;
     var Lname = request.body.Lname;
     var Phone = request.body.Phone;
-    var Address = request.Address;
+    var Address = request.body.Address;
+    console.log(No, Fname, Lname, Phone, Address);
+    var query1 = `insert into Customer(No,Fname,Lname,Phone,Address)
+    values(${No},'${Fname}','${Lname}',${Phone},'${Address}')`;
 
-    var query = `insert into Customer(No,Fname,Lname,Phone,Address)
-    values('${No}','${Fname}','${Lname}','${Phone}','${Address}')`;
-
-    database.query(query, function (error, data) {
+    database.query(query1, function (error, data) {
         if (error) {
             throw error;
         }
         else {
-            response.redirect("/samples1");
+            response.redirect("/sample");
+
         }
     })
 
 });
 
-router.get("/ad", function (request, response, next) {
-    response.send("Add sample data");
+router.get("/edit/:id", function (request, response, next) {
+    var id = request.params.id;
+    var query2 = `select *from Customer where No=${id}`;
+    database.query(query2, function (error, data) {
+        response.render('edit', {
+            title: 'Edit SQL Data',
+            action: 'edit1',
+            Data: data.recordset[0],
+        });
+    });
 });
+response.send("Add sample data");
+
+router.post('edit/:id', function (request, response, next) {
+    var No = request.params.No;
+    var Fname = request.body.Fname;
+    var Lname = request.body.Lname;
+    var Phone = request.body.Phone;
+    var Address = request.body.Address;
+
+    var query3 = `update Customer set Fname='${Fname}', Lname='${Lname}', Phone='${Phone}', 'Address='${Address}' where No=${No}'`;
+})
+
 
 module.exports = router;

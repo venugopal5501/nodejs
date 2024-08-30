6392ed0a8101121fd9055981eb86ccda106b67ef
venugopal5501 <venugopal14082002@gmail.com>
2024-08-30 18:13:22 +0530

    yes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var flash=require('connect-flash');
+app.use(session({secret: 'webslesson',
+  cookie: {maxAge : 60000},
+  resave: false
+}));
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,6 +33,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/sample',samplerouter);
+
+app.use(flash());
 
 
 // catch 404 and forward to error handler
